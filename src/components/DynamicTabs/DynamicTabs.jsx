2d487efc4c666189b63d6a62a0cc1c14435edfa1
Sumair Ahmed <sumair.ahmed@koderlabs.com>
2023-06-15 21:14:27 +0500

    slice work

--- src/components/DynamicTabs/DynamicTabs.jsx
+++ src/components/DynamicTabs/DynamicTabs.jsx
@@ -1,100 +1,99 @@
-import { useState } from 'react';
-import Image from 'next/image'
-import Link from 'next/link';
-import { tabsData } from '@/utils/tabs.data';
-import styles from './DynamicTabs.module.css'
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { tabsData } from "@/utils/tabs.data";
+import styles from "./DynamicTabs.module.css";
 
 const DynamicTabs = ({ tabsData }) => {
-    const [activeTab, setActiveTab] = useState(tabsData[0].id);
+  const [activeTab, setActiveTab] = useState(tabsData[0].id);
 
-    const handleTabClick = (tabId) => {
-        setActiveTab(tabId);
-    };
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
 
-    return (
-        <div>
-            <div className={styles['tabs-sec']}>
-                <ul className={`${styles['nav']} ${styles['nav-tabs']}`}>
-                    {tabsData.map((tab) => (
-                        // eslint-disable-next-line react/jsx-key
-                        <div className={styles['li-box']}>
+  const tabsDataSlice = tabsData.slice(0, 5);
 
-                            <li
-                                key={tab.id}
-                                className={`${styles['tab']} ${activeTab === tab.id ? `${styles['active']}` : ''}`}
-                                onClick={() => handleTabClick(tab.id)}
-                            >
-
-                                <div className={styles['icon-box']}>
-                                    <Image
-                                        src={tab.titleicon}
-                                        alt=""
-                                        width={54}
-                                        height={54}
-                                        loading='lazy'
-                                    />
-                                </div>
-                                <span className={styles['title']}>{tab.title}</span>
-
-                            </li>
-
-                        </div>
-                    ))}
-                </ul>
+  return (
+    <div>
+      <div className={styles["tabs-sec"]}>
+        <ul className={`${styles["nav"]} ${styles["nav-tabs"]}`}>
+          {tabsDataSlice.map((tab) => (
+            // eslint-disable-next-line react/jsx-key
+            <div className={styles["li-box"]}>
+              <li
+                key={tab.id}
+                className={`${styles["tab"]} ${
+                  activeTab === tab.id ? `${styles["active"]}` : ""
+                }`}
+                onClick={() => handleTabClick(tab.id)}
+              >
+                <div className={styles["icon-box"]}>
+                  <Image
+                    src={tab.titleicon}
+                    alt=""
+                    width={54}
+                    height={54}
+                    loading="lazy"
+                  />
+                </div>
+                <span className={styles["title"]}>{tab.title}</span>
+              </li>
             </div>
-            <div className={styles['tab-content']}>
-                {tabsData.map((tab) => (
-                    <div
-                        key={tab.id}
-                        className={`${styles['content']} ${activeTab === tab.id ? `${styles['active']}` : ''}`}
-                    >
-
-                        <div className={styles['tab-pane']}>
-                            <div className='contanier'>
-                                <div className='row'>
-                                    <div className='col-md-6'>
-                                        <div className={styles['image-col']}>
-                                            <Image
-                                                className={styles['first-img']}
-                                                src={tab.fimage}
-                                                alt=""
-                                                width={566}
-                                                height={550}
-                                                loading='lazy'
-                                            />
-                                            <div className={styles['second-img']}>
-                                                <Image
-                                                    src={tab.simage}
-                                                    alt=""
-                                                    width={221}
-                                                    height={215}
-                                                    loading='lazy'
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className='col-md-6'>
-
-                                        <div className={styles['content_service']}>
-                                            <h3 className={styles['title']}>{tab.title}</h3>
-                                            <div className={styles['content-p']}>
-                                                {tab.content}
-                                                {/* <p>{tab.content2}</p> */}
-                                            </div>
-                                            <div className={styles['btns-cus']}>
-                                                <Link href="/our-services">Read More</Link>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+          ))}
+        </ul>
+      </div>
+      <div className={styles["tab-content"]}>
+        {tabsDataSlice.map((tab) => (
+          <div
+            key={tab.id}
+            className={`${styles["content"]} ${
+              activeTab === tab.id ? `${styles["active"]}` : ""
+            }`}
+          >
+            <div className={styles["tab-pane"]}>
+              <div className="contanier">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className={styles["image-col"]}>
+                      <Image
+                        className={styles["first-img"]}
+                        src={tab.fimage}
+                        alt=""
+                        width={566}
+                        height={550}
+                        loading="lazy"
+                      />
+                      <div className={styles["second-img"]}>
+                        <Image
+                          src={tab.simage}
+                          alt=""
+                          width={221}
+                          height={215}
+                          loading="lazy"
+                        />
+                      </div>
                     </div>
-                ))}
+                  </div>
+                  <div className="col-md-6">
+                    <div className={styles["content_service"]}>
+                      <h3 className={styles["title"]}>{tab.title}</h3>
+                      <div className={styles["content-p"]}>
+                        {tab.content}
+                        {/* <p>{tab.content2}</p> */}
+                      </div>
+                      <div className={styles["btns-cus"]}>
+                        <Link href="/our-services">Read More</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DynamicTabs;
--- src/utils/tabs.data.js
+++ src/utils/tabs.data.js
@@ -1,77 +1,144 @@
 export const tabsData = [
   {
     id: 1,
-    title: 'Installation',
-    titleicon: '/images/worker.png',
+    title: "Installation",
+    titleicon: "/images/worker.png",
     content: (
       <span>
-        <p>At <strong>Lighthouse Services</strong>, we understand that proper installation is crucial to the success of any sign project. That’s why we offer comprehensive installation services. We ensure that your signs are assembled safely, efficiently, and to your specifications. Our team has experience and skills in working with various sign types and materials. They have the tools and knowledge to install your signs to the highest standards. We take pride in our attention to detail and commitment to quality and strive to make the installation process as hassle-free as possible for our customers.</p>
-        <p>Connect with us for more details on our installation services and how we can help you bring your sign project to life!</p>
+        <p>
+          At <strong>Lighthouse Services</strong>, we understand that proper
+          installation is crucial to the success of any sign project. That’s why
+          we offer comprehensive installation services. We ensure that your
+          signs are assembled safely, efficiently, and to your specifications.
+          Our team has experience and skills in working with various sign types
+          and materials. They have the tools and knowledge to install your signs
+          to the highest standards. We take pride in our attention to detail and
+          commitment to quality and strive to make the installation process as
+          hassle-free as possible for our customers.
+        </p>
+        <p>
+          Connect with us for more details on our installation services and how
+          we can help you bring your sign project to life!
+        </p>
       </span>
-      ),
-    fimage: '/images/services1.png',
-    simage: '/images/2-min.jpg',
-    link: '/our-services'
+    ),
+    fimage: "/images/services1.png",
+    simage: "/images/2-min.jpg",
+    link: "/our-services",
   },
 
   {
     id: 2,
-    title: 'Maintenance',
-    titleicon: '/images/tools.png',
+    title: "Maintenance",
+    titleicon: "/images/tools.png",
     content: (
       <span>
-        <p>At <strong>Lighthouse Services</strong>, we know that regular maintenance is necessary to keep your signs looking fantastic and operating as they should. That’s why we provide maintenance services in order to keep your signs in top shape at all times. Burnt-out bulbs, frayed wires, and weather damage are all issues that commonly affect signage, and our experienced team is skilled at identifying and fixing them. We also offer regular cleaning and touch-up services to ensure your signs always look their best. With our maintenance services, you can have peace of mind knowing that your signs are being cared for by a company committed to delivering exceptional quality and service.</p>
-        <p>Connect with us for more details on our maintenance services and how we can help you protect your investment!</p>
+        <p>
+          At <strong>Lighthouse Services</strong>, we know that regular
+          maintenance is necessary to keep your signs looking fantastic and
+          operating as they should. That’s why we provide maintenance services
+          in order to keep your signs in top shape at all times. Burnt-out
+          bulbs, frayed wires, and weather damage are all issues that commonly
+          affect signage, and our experienced team is skilled at identifying and
+          fixing them. We also offer regular cleaning and touch-up services to
+          ensure your signs always look their best. With our maintenance
+          services, you can have peace of mind knowing that your signs are being
+          cared for by a company committed to delivering exceptional quality and
+          service.
+        </p>
+        <p>
+          Connect with us for more details on our maintenance services and how
+          we can help you protect your investment!
+        </p>
       </span>
     ),
-    fimage: '/images/services2.png',
-    simage: '/images/2-min.jpg',
-    link: '/our-services'
+    fimage: "/images/services2.png",
+    simage: "/images/2-min.jpg",
+    link: "/our-services",
   },
 
   {
     id: 3,
-    title: 'Sign Permitting & Site Survey Services',
-    titleicon: '/images/scan.png',
+    title: "Sign Permitting & Site Survey Services",
+    titleicon: "/images/scan.png",
     content: (
       <span>
-        <p>At <strong>Lighthouse Services</strong>, we know how challenging it can be to go through the sign-permitting procedure. That’s why we offer comprehensive sign-permitting services. We help ensure that your sign project complies with all local regulations and restrictions. Our skilled team can help you with everything. From preparing the necessary documents and plans to submit the permit application. We handle everything throughout the navigating approval process. We also offer site survey services to ensure your sign is installed in the best possible location. We take into account factors such as visibility, accessibility, and local zoning requirements.</p>
-        <p>Connect with us for more details on our sign permitting and site survey services.</p>
+        <p>
+          At <strong>Lighthouse Services</strong>, we know how challenging it
+          can be to go through the sign-permitting procedure. That’s why we
+          offer comprehensive sign-permitting services. We help ensure that your
+          sign project complies with all local regulations and restrictions. Our
+          skilled team can help you with everything. From preparing the
+          necessary documents and plans to submit the permit application. We
+          handle everything throughout the navigating approval process. We also
+          offer site survey services to ensure your sign is installed in the
+          best possible location. We take into account factors such as
+          visibility, accessibility, and local zoning requirements.
+        </p>
+        <p>
+          Connect with us for more details on our sign permitting and site
+          survey services.
+        </p>
       </span>
-      ),
-    fimage: '/images/services3.png',
-    simage: '/images/2-min.jpg',
-    link: '/our-services'
+    ),
+    fimage: "/images/services3.png",
+    simage: "/images/2-min.jpg",
+    link: "/our-services",
   },
 
   {
     id: 4,
-    title: 'Lighted Signs, LED Signs, And Sign Monuments',
-    titleicon: '/images/Construction-Plan.png',
+    title: "Lighted Signs, LED Signs, And Sign Monuments",
+    titleicon: "/images/Construction-Plan.png",
     content: (
       <span>
-        <p>At <strong>Lighthouse Services</strong>, we specialize in creating custom lighted signs, LED signs, and sign monuments. Our designers and technicians use the latest tech and highest-quality materials to create signs that are both visually stunning and long-lasting. Whether you need an illuminated storefront sign, an attention-grabbing LED message board, or a durable monument sign to welcome visitors to your property, we have got you! We help your business stand out and make a lasting impression through our signage solution.</p>
-        <p>We can create a custom solution that meets your specific needs and budget. From design and fabrication to installation and maintenance, we are your full-service sign center for all your lighted, LED, and monument sign needs.</p>
+        <p>
+          At <strong>Lighthouse Services</strong>, we specialize in creating
+          custom lighted signs, LED signs, and sign monuments. Our designers and
+          technicians use the latest tech and highest-quality materials to
+          create signs that are both visually stunning and long-lasting. Whether
+          you need an illuminated storefront sign, an attention-grabbing LED
+          message board, or a durable monument sign to welcome visitors to your
+          property, we have got you! We help your business stand out and make a
+          lasting impression through our signage solution.
+        </p>
+        <p>
+          We can create a custom solution that meets your specific needs and
+          budget. From design and fabrication to installation and maintenance,
+          we are your full-service sign center for all your lighted, LED, and
+          monument sign needs.
+        </p>
       </span>
-      ),
-    fimage: '/images/services4.png',
-    simage: '/images/2-min.jpg',
-    link: '/our-services'
+    ),
+    fimage: "/images/services4.png",
+    simage: "/images/2-min.jpg",
+    link: "/our-services",
   },
 
   {
     id: 5,
-    title: 'Sign Design & Assist',
-    titleicon: '/images/blueprint.png',
+    title: "Sign Design & Assist",
+    titleicon: "/images/blueprint.png",
     content: (
-    <span>
-      <p><strong>Lighthouse Services</strong> helps you create the perfect custom sign for your business. Our team of designers works closely with you to understand your brand and design preferences. They take note of any specific requirements or restrictions for your sign. We use the latest design software and techniques to create a digital rendering of your sign. We also offer sign assist services to help you navigate the sometimes-complex sign-permitting process.</p>
-      <p>When you use our sign design and assistance services, you can be sure your personalized sign will be expertly designed, fabricated, and installed with the utmost care and attention to detail.</p>
+      <span>
+        <p>
+          <strong>Lighthouse Services</strong> helps you create the perfect
+          custom sign for your business. Our team of designers works closely
+          with you to understand your brand and design preferences. They take
+          note of any specific requirements or restrictions for your sign. We
+          use the latest design software and techniques to create a digital
+          rendering of your sign. We also offer sign assist services to help you
+          navigate the sometimes-complex sign-permitting process.
+        </p>
+        <p>
+          When you use our sign design and assistance services, you can be sure
+          your personalized sign will be expertly designed, fabricated, and
+          installed with the utmost care and attention to detail.
+        </p>
       </span>
     ),
-    fimage: '/images/services5.png',
-    simage: '/images/2-min.jpg',
-    link: '/our-services'
+    fimage: "/images/services5.png",
+    simage: "/images/2-min.jpg",
+    link: "/our-services",
   },
-
 ];
